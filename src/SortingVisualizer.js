e365dfabab911c6323ac07dae633969ce940239f
Tunaberk Almaci <almacitunaberk@gmail.com>
2022-06-23 18:56:24 -0400

    Automatic resetting of array when the window size changes added

--- src/SortingVisualizer.js
+++ src/SortingVisualizer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { useState, useEffect } from 'react';
 import Header from './Header';
 import './SortingVisualizer.css';
@@ -6,18 +6,22 @@ import './SortingVisualizer.css';
 const SortingVisualizer = () => {
   const [array, setArray] = useState([]);
   const [arraySize, setArraySize] = useState(300);
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
 
   const resetArray = () => {
     const newArray = [];
     for (let i = 0; i < arraySize; i++) {
-      newArray.push(generateRandomNubmer(10, 1000));
+      newArray.push(generateRandomNubmer(10, viewportHeight - 100));
     }
     setArray(newArray);
   };
 
   useEffect(() => {
+    window.addEventListener('resize', () => {
+      setViewportHeight(window.innerHeight);
+    });
     resetArray();
-  }, []);
+  }, [window.innerHeight]);
 
   const generateRandomNubmer = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
