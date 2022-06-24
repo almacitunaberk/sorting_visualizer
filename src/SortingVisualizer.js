d6e90355fd79c6e5a94adda5a97dc93e6c69f3c7
Tunaberk Almaci <almacitunaberk@gmail.com>
2022-06-23 20:09:32 -0400

    Refactored - Array bar height caÃlcÄulated based on percentage with the largest value

--- src/SortingVisualizer.js
+++ src/SortingVisualizer.js
@@ -7,22 +7,26 @@ import './SortingVisualizer.css';
 const SortingVisualizer = () => {
   const [array, setArray] = useState([]);
   const [arraySize, setArraySize] = useState(300);
-  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+  const [largestValue, setLargestValue] = useState(10);
 
   const resetArray = () => {
     const newArray = [];
+    let tempLargestVal = 0;
     for (let i = 0; i < arraySize; i++) {
-      newArray.push(generateRandomNubmer(10, viewportHeight - 100));
+      const randomNumber = generateRandomNubmer(10, 1000);
+      newArray.push(randomNumber);
+      if (randomNumber > tempLargestVal) {
+        tempLargestVal = randomNumber;
+      }
     }
     setArray(newArray);
+    setLargestValue(tempLargestVal);
+    console.log(newArray.length);
   };
 
   useEffect(() => {
-    window.addEventListener('resize', () => {
-      setViewportHeight(window.innerHeight);
-    });
     resetArray();
-  }, [window.innerHeight, arraySize]);
+  }, [arraySize]);
 
   const generateRandomNubmer = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -44,7 +48,7 @@ const SortingVisualizer = () => {
       />
       <div className="bar__container">
         {array.map((value, idx) => (
-          <div className="array-bar" key={idx} style={{ height: `${value}px` }}></div>
+          <div className="array-bar" key={idx} style={{ height: `${(value / largestValue) * 100}%` }}></div>
         ))}
       </div>
     </>
